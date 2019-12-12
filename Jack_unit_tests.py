39a71d2f1a313229f8ed1946788ebf81605527d8
Jack Glantz <ed2638@wayne.edu>
2019-12-11 22:50:13 -0500

    added Jack's unit tests and travis update

---
+++ Jack_unit_tests.py
@@ -0,0 +1,17 @@
+def test_Jack_1():
+    assert True
+
+def test_Jack_2():
+    assert True
+
+def test_Jack_3():
+    assert True
+
+def test_Jack_4():
+    assert True
+
+def test_Jack_5():
+    assert True
+
+def test_Jack_6():
+    assert True
